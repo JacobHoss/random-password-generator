4915fe441893f04789d3e04a61b9e57e4823d837
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-06-25 12:49:42 -0400

    added some functionality. still troubleshooting issues.

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
  
-function generatePassword() {
-var charCount = prompt("How many characters would you like your password to have? It can have a length from 8 to 128.").trim();
-var lowerCase = confirm("Would you like to include lower case letters?");
-var upperCase = confirm("Would you like to include upper case letters?");
-var Numeric = confirm("Would you like to include numeric characters?");
-var specialCharacters = confirm("Would you like to include Special Characters?");
-var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'X', 'Y', 'Z']
-var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 'x', 'y', 'z']
-var spchar = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', '|', '"', ':', ';', '<', '>', ',', '.', '?']
-
-console.log(lower[15]);
-console.log(spchar[18]);
-console.log(upper[20])
-
-if (charCount >= 50 || lowerCase === true || upperCase === true || Numeric === true) {
-    alert("Good luck remembering this password!")
+function generatePassword( len ) {
+    var charCount = prompt("How many characters would you like your password to have? It can have a length from 8 to 128.").trim();
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+    var loweryn = confirm("Would you like to include lower case letters?");
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var upperyn = confirm("Would you like to include upper case letters?");
+    var numeric = '0123456789';
+    var numyn = confirm("Would you like to include numeric characters?");
+    var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    var puncyn = confirm("Would you like to include Special Characters?");
+    var password = "";
+    var character = "";
+    var crunch = true;
+ 
+if (loweryn === false && numyn === false && upperyn === false && puncyn === false) {
+alert("If you chose No for everything, then we won't give you a password...");
 }
+ 
+    while( password.length<charCount ) {
+        entity1 = Math.ceil(lower.length * Math.random()*Math.random());
+        entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
+        entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
+        entity4 = Math.ceil(upper.length * Math.random()*Math.random());
+        hold = lower.charAt( entity1 ) 
+        hold = (password.length%2==0)?(hold):(hold);
+        character += hold;
+        if (loweryn === true) {character += lower.charAt ( entity1 );}
+        if (numyn === true) {character += numeric.charAt( entity2 );}
+        if (puncyn === true) {character += punctuation.charAt( entity3 );}
+        if (upperyn === true) {character += upper.charAt( entity4 );}
+        password = character;
+    }
+    password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+    return password.substr(0,len);
 }
+ 
+ 
  
 // Write password to the #password input
 function writePassword() {
@@ -31,5 +50,3 @@ function writePassword() {
  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
- 
-
